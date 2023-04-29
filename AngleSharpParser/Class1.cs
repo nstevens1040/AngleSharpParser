1915958290886cd9690d743fd70411637abb766e
nstevens <nstevens@BING-BONG>
2023-04-28 19:55:30 -0500

    Add project files.

---
+++ AngleSharpParser/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace AngleSharp
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AngleSharp;
+    using AngleSharp.Dom;
+    using System.Reflection;
+    using AngleSharpParser.Properties;
+    public class Parser
+    {
+        public Parser()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                string resourceName = new AssemblyName(args.Name).Name + ".dll";
+                string resource = Array.Find(this.GetType().Assembly.GetManifestResourceNames(), element => element.EndsWith(resourceName));
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
+        }
+        public IDocument GetDomDocument(string HtmlString)
+        {
+            Task<IDocument> doc = Task.Factory.StartNew(async () =>
+            {
+                IBrowsingContext context = BrowsingContext.New(Configuration.Default);
+                IDocument d = await context.OpenAsync(req => req.Content(HtmlString));
+                return d;
+            }).Result;
+            return doc.Result;
+        }
+    }
+}
